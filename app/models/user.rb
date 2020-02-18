f4ca23f00efb92182195208f3c7a689d13524576
Kevin McGrevey <niv3kmcg@gmail.com>
2020-02-18 16:09:50 -0700

    Finish model testing for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-  validates_presence_of :name, :address, :city, :state, :zip, :email, :password, :password_confirmation
+  validates_presence_of :name, :address, :city, :state, :zip, :email, :password
 
   validates_uniqueness_of :email
 
   has_secure_password
+
+  enum role: %w(user merchant_employee admin visitor)
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,8 +9,41 @@ describe User, type: :model do
     it {should validate_presence_of :zip}
     it {should validate_presence_of :email}
     it {should validate_presence_of :password}
-    it {should validate_presence_of :password_confirmation}
 
     it {should validate_uniqueness_of :email}
+  end
+
+  describe "roles" do
+    it "can be created as an admin" do
+      user = User.create(name: "penelope",
+                          address: "123 W",
+                          city: "a",
+                          state: "IN",
+                          zip: 12345,
+                          email: "a",
+                         password: "boom",
+
+                         role: 2)
+
+      expect(user.role).to eq("admin")
+      expect(user.admin?).to be_truthy
+    end
+
+    it "can be created as a default user" do
+      user = User.create(name: "penelope",
+                          address: "123 W",
+                          city: "a",
+                          state: "IN",
+                          zip: 12345,
+                          email: "a",
+                         password: "boom",
+
+                         role: 0)
+
+
+
+      expect(user.role).to eq("user")
+      expect(user.user?).to be_truthy
+    end
   end
 end
