134333787d21fe0a5b384060e932518b07bc96fe
Kevin McGrevey <niv3kmcg@gmail.com>
2020-02-18 15:54:11 -0700

    Remove commented out code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  # validates :username, uniqueness: true, presence: true
   validates_presence_of :name, :address, :city, :state, :zip, :email, :password, :password_confirmation
 
   validates_uniqueness_of :email
-  #
+
   has_secure_password
 end
