1b88786f3b68f1cd32fb70241d3ec377fcadfbb4
Kevin McGrevey <niv3kmcg@gmail.com>
2020-02-20 14:40:52 -0700

    Add test most pouplar and least poular items

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -48,4 +48,49 @@ describe Item, type: :model do
       expect(@chain.no_orders?).to eq(false)
     end
   end
+
+  describe "class methods" do
+    before(:each) do
+      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+
+      @tire = @meg.items.create!(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      @seat = @meg.items.create(name: "Seat", description: "Cushy for your tushy.", price: 199, image: "https://www.rei.com/media/product/153242", inventory: 20)
+      @pump = @meg.items.create(name: "Pump", description: "Not just hot air", price: 70, image: "https://www.rei.com/media/product/152974", inventory: 20)
+      @pedals = @meg.items.create(name: "Pedals", description: "Clipless bliss!", price: 210, image: "https://www.rei.com/media/product/130015", inventory: 20)
+      @helmet = @meg.items.create(name: "Helmet", description: "Safety Third!", price: 100, image: "https://www.rei.com/media/product/153004", inventory: 20)
+
+      @pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+      @dog_bone = @brian.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+      @carrier = @brian.items.create(name: "Carrier", description: "Home away from home", price: 70, image: "https://s7d2.scene7.com/is/image/PetSmart/5168894?$sclp-prd-main_large$", inventory: 32)
+      @bed = @brian.items.create(name: "Doggie Bed", description: "Soo plush and comfy!", price: 40, image: "https://s7d2.scene7.com/is/image/PetSmart/5291324", inventory: 32)
+      @dog_food = @brian.items.create(name: "Bag o Food", description: "Nutrition in bulk", price: 54, image: "https://s7d2.scene7.com/is/image/PetSmart/5149892", inventory: 32)
+      @collar = @brian.items.create(name: "Dog Collar", description: "Choker", price: 18, image: "https://s7d2.scene7.com/is/image/PetSmart/5169886", inventory: 32)
+      @brush = @brian.items.create(name: "Dog Brush", description: "Detangle those curls", price: 12, image: "https://s7d2.scene7.com/is/image/PetSmart/5280398", inventory: 32)
+
+
+      @order1 = Order.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+
+      @order1.item_orders.create!(item: @tire, price: @tire.price, quantity: 11)
+      @order1.item_orders.create!(item: @seat, price: @seat.price, quantity: 10)
+      @order1.item_orders.create!(item: @pump, price: @pump.price, quantity: 9)
+      @order1.item_orders.create!(item: @pedals, price: @pedals.price, quantity: 8)
+      @order1.item_orders.create!(item: @helmet, price: @helmet.price, quantity: 7)
+
+      @order1.item_orders.create!(item: @brush, price: @brush.price, quantity: 1)
+      @order1.item_orders.create!(item: @collar, price: @collar.price, quantity: 2)
+      @order1.item_orders.create!(item: @dog_food, price: @dog_food.price, quantity: 3)
+      @order1.item_orders.create!(item: @bed, price: @bed.price, quantity: 4)
+      @order1.item_orders.create!(item: @carrier, price: @carrier.price, quantity: 5)
+
+    end
+
+    it '.most_poular' do
+      expect(Item.most_popular(5)).to eq([@tire, @seat, @pump, @pedals, @helmet])
+    end
+
+    it '.least_poular' do
+      expect(Item.least_popular(5)).to eq([@brush, @collar, @dog_food, @bed, @carrier])
+    end
+  end
 end
