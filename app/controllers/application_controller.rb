3925834b5963483be351c3db884b961c84b81914
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-02-19 19:09:13 -0700

    Add user profile page show information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-	before_action :current_user
+  # before_action :current_user
   protect_from_forgery with: :exception
 
   helper_method :cart, :current_user, :current_admin?, :current_merchant?
@@ -8,12 +8,11 @@ class ApplicationController < ActionController::Base
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
   end
 
-	def current_user
-	 	return false unless session[:user_id]
-	 	@current_user ||= User.find(session[:user_id])
- 	end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
-	# def current_admin?
+  # def current_admin?
   #   current_user && current_user.admin?
   # end
 
@@ -22,7 +21,6 @@ class ApplicationController < ActionController::Base
   # end
 
   def current_default?
-    current_user && current_user.default?
+    current_user&.default?
   end
-
 end
