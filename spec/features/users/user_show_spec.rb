a3dd6f69c143b712ccb947c138efa45c790ded2e
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-02-19 18:30:49 -0700

    Add before block for test of user profile page

--- spec/features/users/user_show_spec.rb
+++ spec/features/users/user_show_spec.rb
@@ -1,1 +1,21 @@
 require 'rails_helper'
+
+RSpec.describe 'As a visitor' do
+  describe 'when I visit my user profile' do
+    before :each do
+      @user1 = User.create(
+        name: 'Steve',
+        address: '123 Street Road',
+        city: 'City Name', state: 'CO',
+        zip: 12345,
+        email: 'example@example.com',
+        password: 'password1',
+        role: 0
+      )
+    end
+
+    it 'can see all of my information' do
+
+    end
+  end
+end
