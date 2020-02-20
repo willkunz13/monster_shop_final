cb1dd814263927dfbd129d840a83523ce064874d
sasloan <sebastiansloan94@gmail.com>
2020-02-20 12:46:42 -0700

    login works for all users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,56 +1,56 @@
 class SessionsController < ApplicationController
-  def new
-    unless session[:user_id].nil?
-      flash[:notice] = "You are already logged in, to log out please select 'Log Out' at the top of your page."
-      redirect_to '/user/profile'
-    end
-  end
-
   # def new
-  #   if !session[:user_id].nil?
-  #     redirect_to "/admin/dashboard"
-  #     flash[:notice] = "You are already logged in as #{current_user.name}, to log out please select 'Log Out' at the top of your page."
-  #   elsif current_merchant?
-  #     redirect_to "/merchant/dashboard"
-  #     flash[:notice] = "You are already logged in as #{current_user.name}, to log out please select 'Log Out' at the top of your page."
-  #   elsif current_default?
-  #     redirect_to "/profile"
-  #     flash[:notice] = "You are already logged in as #{current_user.name}, to log out please select 'Log Out' at the top of your page."
+  #   unless session[:user_id].nil?
+  #     flash[:notice] = "You are already logged in, to log out please select 'Log Out' at the top of your page."
+  #     redirect_to '/user/profile'
   #   end
   # end
 
-  def create
-    user = User.find_by(email: params[:email])
-    if !user.nil? && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      flash[:success] = "#{user.name}, has been successfully logged in!"
-      redirect_to '/user/profile'
-    else
-      flash.now[:notice] = 'Your Log in attempt failed, Wrong email or password'
-      render :new
+  def new
+    if !session[:user_id].nil?
+      redirect_to "/admin/dashboard"
+      flash[:notice] = "You are already logged in as #{current_user.name}, to log out please select 'Log Out' at the top of your page."
+    elsif current_merchant?
+      redirect_to "/merchant/dashboard"
+      flash[:notice] = "You are already logged in as #{current_user.name}, to log out please select 'Log Out' at the top of your page."
+    elsif current_default?
+      redirect_to "/profile"
+      flash[:notice] = "You are already logged in as #{current_user.name}, to log out please select 'Log Out' at the top of your page."
     end
   end
 
   # def create
   #   user = User.find_by(email: params[:email])
-  #   if user != nil && user.authenticate(params[:password])
+  #   if !user.nil? && user.authenticate(params[:password])
   #     session[:user_id] = user.id
   #     flash[:success] = "#{user.name}, has been successfully logged in!"
-  #     if user.admin?
-  #       redirect_to '/admin/dashboard'
-  #       flash[:success] = "#{user.name}, has been successfully logged in!"
-  #     elsif user.merchant?
-  #       redirect_to '/merchant/dashboard'
-  #       flash[:success] = "#{user.name}, has been successfully logged in!"
-  #     else user.default?
-  #       redirect_to '/profile'
-  #       flash[:success] = "#{user.name}, has been successfully logged in!"
-  #     end
+  #     redirect_to '/user/profile'
   #   else
-  #     flash.now[:notice] = "Your Log in attempt failed, Wrong email or password"
+  #     flash.now[:notice] = 'Your Log in attempt failed, Wrong email or password'
   #     render :new
   #   end
   # end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user != nil && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "#{user.name}, has been successfully logged in!"
+      if user.admin?
+        redirect_to '/admin/dashboard'
+        flash[:success] = "#{user.name}, has been successfully logged in!"
+      elsif user.merchant_employee?
+        redirect_to '/merchant/dashboard'
+        flash[:success] = "#{user.name}, has been successfully logged in!"
+      else user.default?
+        redirect_to '/profile'
+        flash[:success] = "#{user.name}, has been successfully logged in!"
+      end
+    else
+      flash.now[:notice] = "Your Log in attempt failed, Wrong email or password"
+      render :new
+    end
+  end
 
   def destroy
     session.delete(:user_id)
--- spec/features/sessions/login_spec.rb
+++ spec/features/sessions/login_spec.rb
@@ -81,166 +81,166 @@ describe 'As a User' do
   end
 end
 
-# describe 'As a Merchant' do
-#   describe 'When I go to the welcome page I see a link to log in' do
-#     before :each do
-#       @merchant = User.create!(name: "Merchant", address: "123 merchant ave.", city: "City of Townsville", state: "Nv", zip: "39433", email: "merchant@gmail.com", password: "merchant", role: 1)
-#     end
-#
-#     it 'when I click that link I am taken to a log in form to log in' do
-#
-#       visit '/welcome'
-#
-#       expect(current_path).to eq("/welcome")
-#
-#       expect(page).to have_link("Log In")
-#
-#       click_on "Log In"
-#
-#       expect(current_path).to eq("/login")
-#
-#       expect(page).to have_content("Email")
-#       expect(page).to have_content("Password")
-#
-#       within '#log_in_form' do
-#         fill_in :email, with: @merchant.email
-#         fill_in :password, with: @merchant.password
-#
-#         click_on "Log In"
-#       end
-#
-#       expect(current_path).to eq("/merchant/dashboard")
-#     end
+describe 'As a Merchant' do
+  describe 'When I go to the welcome page I see a link to log in' do
+    before :each do
+      @merchant = User.create!(name: "Merchant", address: "123 merchant ave.", city: "City of Townsville", state: "Nv", zip: "39433", email: "merchant@gmail.com", password: "merchant", role: 1)
+    end
 
-# it 'If I click on the log in button when I am already logged in then I recive a flash message and am sent back to the profile page' do
-#   visit '/welcome'
-#
-#   expect(current_path).to eq('/welcome')
-#
-#   click_on "Log In"
-#
-#   expect(current_path).to eq('/login')
-#
-#   within '#log_in_form' do
-#     fill_in :email, with: @merchant.email
-#     fill_in :password, with: @merchant.password
-#
-#     click_on 'Log In'
-#   end
-#
-#   expect(current_path).to eq("/merchant_employee/dashboard")
-#
-#   visit '/login'
+    it 'when I click that link I am taken to a log in form to log in' do
 
-#   expect(page).to have_content("You are already logged in, to log out please select 'Log Out' at the top of your page.")
-# end
-#
-#     it 'If I enter the wrong infromation then I am redirected back to the form with a error message' do
-#
-#       visit '/welcome'
-#
-#       expect(current_path).to eq("/welcome")
-#
-#       expect(page).to have_link("Log In")
-#
-#       click_on "Log In"
-#
-#       expect(current_path).to eq("/login")
-#
-#       expect(page).to have_content("Email")
-#       expect(page).to have_content("Password")
-#
-#       within '#log_in_form' do
-#         fill_in :email, with: @merchant.email
-#         fill_in :password, with: "user"
-#
-#         click_on "Log In"
-#       end
-#
-#       expect(page).to have_content("Your Log in attempt failed, Wrong email or password")
-#       expect(current_path).to eq("/login")
-#     end
-#   end
-# end
-#
-# describe 'As a Admin' do
-#   describe 'When I go to the welcome page I see a link to log in' do
-#     before :each do
-#       @admin = User.create!(name: "Admin", address: "123 Admin cir.", city: "The provinance of holeville", state: "Ca", zip: "83845", email: "admin@gmail.com", password: "admin", role: 2)
-#     end
-#
-#     it 'when I click that link I am taken to a log in form to log in' do
-#
-#       visit '/welcome'
-#
-#       expect(current_path).to eq("/welcome")
-#
-#       expect(page).to have_link("Log In")
-#
-#       click_on "Log In"
-#
-#       expect(current_path).to eq("/login")
-#
-#       expect(page).to have_content("Email")
-#       expect(page).to have_content("Password")
-#
-#       within '#log_in_form' do
-#         fill_in :email, with: @admin.email
-#         fill_in :password, with: @admin.password
-#
-#         click_on "Log In"
-#       end
-#
-#       expect(current_path).to eq("/admin/dashboard")
-#     end
-#
-#     it 'If I click on the log in button when I am already logged in then I recive a flash message and am sent back to the profile page' do
-#       visit '/welcome'
-#
-#       expect(current_path).to eq('/welcome')
-#
-#       click_on "Log In"
-#
-#       expect(current_path).to eq('/login')
-#
-#       within '#log_in_form' do
-#         fill_in :email, with: @admin.email
-#         fill_in :password, with: @admin.password
-#
-#         click_on 'Log In'
-#       end
-#
-#       expect(current_path).to eq("/admin/dashboard")
-#
-#       visit '/login'
+      visit '/welcome'
 
-#       expect(page).to have_content("You are already logged in, to log out please select 'Log Out' at the top of your page.")
-#     end
+      expect(current_path).to eq("/welcome")
 
-#     it 'If I enter the wrong infromation then I am redirected back to the form with a error message' do
-#
-#       visit '/welcome'
-#
-#       expect(current_path).to eq("/welcome")
-#
-#       expect(page).to have_link("Log In")
-#
-#       click_on "Log In"
-#
-#       expect(current_path).to eq("/login")
-#
-#       expect(page).to have_content("Email")
-#       expect(page).to have_content("Password")
-#
-#       within '#log_in_form' do
-#         fill_in :email, with: @admin.email
-#         fill_in :password, with: "merchant"
-#
-#         click_on "Log In"
-#       end
-#
-#       expect(page).to have_content("Your Log in attempt failed, Wrong email or password")
-#       expect(current_path).to eq("/login")
-#     end
-#   end
-# end
+      expect(page).to have_link("Log In")
+
+      click_on "Log In"
+
+      expect(current_path).to eq("/login")
+
+      expect(page).to have_content("Email")
+      expect(page).to have_content("Password")
+
+      within '#log_in_form' do
+        fill_in :email, with: @merchant.email
+        fill_in :password, with: @merchant.password
+
+        click_on "Log In"
+      end
+
+      expect(current_path).to eq("/merchant/dashboard")
+    end
+
+it 'If I click on the log in button when I am already logged in then I recive a flash message and am sent back to the profile page' do
+  visit '/welcome'
+
+  expect(current_path).to eq('/welcome')
+
+  click_on "Log In"
+
+  expect(current_path).to eq('/login')
+
+  within '#log_in_form' do
+    fill_in :email, with: @merchant.email
+    fill_in :password, with: @merchant.password
+
+    click_on 'Log In'
+  end
+
+  expect(current_path).to eq("/merchant/dashboard")
+
+  visit '/login'
+
+  expect(page).to have_content("You are already logged in as Merchant, to log out please select 'Log Out' at the top of your page.")
+end
+
+    it 'If I enter the wrong infromation then I am redirected back to the form with a error message' do
+
+      visit '/welcome'
+
+      expect(current_path).to eq("/welcome")
+
+      expect(page).to have_link("Log In")
+
+      click_on "Log In"
+
+      expect(current_path).to eq("/login")
+
+      expect(page).to have_content("Email")
+      expect(page).to have_content("Password")
+
+      within '#log_in_form' do
+        fill_in :email, with: @merchant.email
+        fill_in :password, with: "user"
+
+        click_on "Log In"
+      end
+
+      expect(page).to have_content("Your Log in attempt failed, Wrong email or password")
+      expect(current_path).to eq("/login")
+    end
+  end
+end
+
+describe 'As a Admin' do
+  describe 'When I go to the welcome page I see a link to log in' do
+    before :each do
+      @admin = User.create!(name: "Admin", address: "123 Admin cir.", city: "The provinance of holeville", state: "Ca", zip: "83845", email: "admin@gmail.com", password: "admin", role: 2)
+    end
+
+    it 'when I click that link I am taken to a log in form to log in' do
+
+      visit '/welcome'
+
+      expect(current_path).to eq("/welcome")
+
+      expect(page).to have_link("Log In")
+
+      click_on "Log In"
+
+      expect(current_path).to eq("/login")
+
+      expect(page).to have_content("Email")
+      expect(page).to have_content("Password")
+
+      within '#log_in_form' do
+        fill_in :email, with: @admin.email
+        fill_in :password, with: @admin.password
+
+        click_on "Log In"
+      end
+
+      expect(current_path).to eq("/admin/dashboard")
+    end
+
+    it 'If I click on the log in button when I am already logged in then I recive a flash message and am sent back to the profile page' do
+      visit '/welcome'
+
+      expect(current_path).to eq('/welcome')
+
+      click_on "Log In"
+
+      expect(current_path).to eq('/login')
+
+      within '#log_in_form' do
+        fill_in :email, with: @admin.email
+        fill_in :password, with: @admin.password
+
+        click_on 'Log In'
+      end
+
+      expect(current_path).to eq("/admin/dashboard")
+
+      visit '/login'
+
+      expect(page).to have_content("You are already logged in as Admin, to log out please select 'Log Out' at the top of your page.")
+    end
+
+    it 'If I enter the wrong infromation then I am redirected back to the form with a error message' do
+
+      visit '/welcome'
+
+      expect(current_path).to eq("/welcome")
+
+      expect(page).to have_link("Log In")
+
+      click_on "Log In"
+
+      expect(current_path).to eq("/login")
+
+      expect(page).to have_content("Email")
+      expect(page).to have_content("Password")
+
+      within '#log_in_form' do
+        fill_in :email, with: @admin.email
+        fill_in :password, with: "merchant"
+
+        click_on "Log In"
+      end
+
+      expect(page).to have_content("Your Log in attempt failed, Wrong email or password")
+      expect(current_path).to eq("/login")
+    end
+  end
+end
