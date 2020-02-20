8c50aaac4b56020b2a3ceb3fe22282e6763d1b68
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-02-19 18:38:29 -0700

    Add test for user show page(profile)

--- spec/features/users/user_show_spec.rb
+++ spec/features/users/user_show_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe 'As a visitor' do
       @user1 = User.create(
         name: 'Steve',
         address: '123 Street Road',
-        city: 'City Name', state: 'CO',
+        city: 'City Name',
+        state: 'CO',
         zip: 12345,
         email: 'example@example.com',
         password: 'password1',
@@ -15,7 +16,20 @@ RSpec.describe 'As a visitor' do
     end
 
     it 'can see all of my information' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
 
+      visit '/profile'
+
+      within '#user_profile_info' do
+        expect(page).to have_content('Name: Steve')
+        expect(page).to have_content('Address: 123 Street Road')
+        expect(page).to have_content("City Name CO, #{12345}")
+        expect(page).to have_content('Email: example@example.com')
+      end
+
+      within '#user_buttons' do
+        expect(page).to have_link('Edit')
+      end
     end
   end
 end
