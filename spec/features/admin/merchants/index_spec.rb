a8d53ba509eaed979cdc2752d066176e3e6eb00f
Kevin McGrevey <niv3kmcg@gmail.com>
2020-02-23 18:06:13 -0700

    Add test to enable and disable Merchant

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe 'As an Admin' do
 	describe 'When I visit the merchants index page.' do
 		before :each do
-			@megs_shop = Merchant.create!(name: "Meg's Dog Shop", address: '123 Dog Rd.', city: 'Hershey', state: 'PA', zip: 80203)
-			@brians_shop = Merchant.create!(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+			@megs_shop = Merchant.create!(name: "Meg's Dog Shop", address: '123 Dog Rd.', city: 'Hershey', state: 'PA', zip: 80203) #status ENABLED
+			@brians_shop = Merchant.create!(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210) #status DISABLED
 
 			@admin = User.create!(name: "Admin", address: "123 Admin cir.", city: "The provinance of holeville", state: "Ca", zip: "83845", email: "admin@gmail.com", password: "admin", role: 2)
 			allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
 	 	end
 
-		it 'I see a list of all the Merchants on the website and there names are a hyperlink to their dashborads.' do
+		it 'I see a list of all the Merchants on the website and their names are a hyperlink to their dashboards.' do
 
 			visit "/admin/merchants"
 
@@ -25,6 +25,44 @@ RSpec.describe 'As an Admin' do
 				click_on "#{@megs_shop.name}"
 
 				expect(current_path).to eq("/admin/merchants/#{@megs_shop.id}")
+			end
+		end
+
+		it 'I can click on a disable button next to any Merchants who are not yet disabled and their status becomes disabled' do
+
+			visit "/admin/merchants"
+
+			within "#merchant-#{@brians_shop.id}" do
+				expect(page).not_to have_button("disable")
+				expect(page).to have_content("Account Status: disabled")
+			end
+
+			within "#merchant-#{@megs_shop.id}" do
+				click_on "disable"
+
+				expect(current_path).to eq("/admin/merchants")
+				expect(page).not_to have_button("disable")
+				expect(page).to have_content("Account Status: disabled")
+				expect(page).to have_content("Account for '#{@megs_shop.name}' is now DISABLED.")
+			end
+		end
+
+		it 'I can click enable button next to any Merchants who are not yet enabled and their status becomes enabled' do
+
+			visit "/admin/merchants"
+
+			within "#merchant-#{@megs_shop.id}" do
+				expect(page).not_to have_button("enable")
+				expect(page).to have_content("Account Status: enabled")
+			end
+
+				within "#merchant-#{@brians_shop.id}" do
+				click_on "enable"
+
+				expect(current_path).to eq("/admin/merchants")
+				expect(page).not_to have_button("enable")
+				expect(page).to have_content("Account Status: enabled")
+				expect(page).to have_content("Account for '#{@brians_shop.name}' is now ENABLED.")
 			end
 		end
 	end
