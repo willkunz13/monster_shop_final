fcf8b4ae8afa28d64905bab29f3af918a3e9db56
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-02-25 11:54:43 -0700

    Fix test in cancel order test

--- spec/features/users/delete_order_spec.rb
+++ spec/features/users/delete_order_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'As a User' do
   describe 'When I visit my profile page' do
     before :each do
-      @mike = Merchant.create!(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80_203)
-      @meg = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
+      @mike = Merchant.create!(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @meg = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @tire = @meg.items.create!(name: 'Gatorskins', description: "They'll never pop!", price: 100, image: 'https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588', inventory: 12)
       @paper = @mike.items.create!(name: 'Lined Paper', description: 'Great for writing on!', price: 20, image: 'https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png', inventory: 3)
       @pencil = @mike.items.create!(name: 'Yellow Pencil', description: 'You can write on paper with it!', price: 2, image: 'https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg', inventory: 100)
@@ -13,6 +13,7 @@ RSpec.describe 'As a User' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
 
       @order_1 = @user1.orders.create!(name: @user1.name, address: 'address', city: 'city', state: 'state', zip: 12345)
+      @order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
 
       visit '/user/profile'
 
@@ -33,6 +34,12 @@ RSpec.describe 'As a User' do
       visit "/user/profile/orders/#{@order_1.id}"
 
       expect(page).to have_content('cancelled')
+
+      visit '/items'
+
+      within "#item-#{@tire.id}" do
+        expect(page).to have_content('Inventory: 14') 
+      end
     end
   end
 end
