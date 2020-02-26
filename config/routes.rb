a28f65db8fdb331bf8c0433301043b4366435d71
Kevin McGrevey <niv3kmcg@gmail.com>
2020-02-26 00:40:40 -0700

    Replace merchant routes with routh helpers

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,9 @@ Rails.application.routes.draw do
   # get '/merchants/new', to: 'merchants#new'
   # get '/merchants/:id', to: 'merchants#show'
   # post '/merchants', to: 'merchants#create'
-  get '/merchants/:id/edit', to: 'merchants#edit'
-  patch '/merchants/:id', to: 'merchants#update'
-  delete '/merchants/:id', to: 'merchants#destroy'
+  # get '/merchants/:id/edit', to: 'merchants#edit'
+  # patch '/merchants/:id', to: 'merchants#update'
+  # delete '/merchants/:id', to: 'merchants#destroy'
 
 	# Items
   get '/items', to: 'items#index'
